c82e57f85bbeaa00b8d50623f7c0478dd3ce9ef0
MIZOCR7 <myfreelancinglife777@gmail.com>
2026-07-08 14:13:36 +0300

    making the setup

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import pygame
 
+pygame.init()
+
+WIDTH = 1280
+HEIGHT = 720
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+surf = pygame.Surface((100,200))
+surf.fill("red")
+
+player_surf = pygame.image.load("images/player.png")
+
+def main():
+  run = True
+
+  while run:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False 
+      
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+          run = False
+    
+    screen.fill('darkgray')
+    screen.blit(player_surf, (100,150))
+    pygame.display.update() 
+  pygame.quit()
+
+if __name__ == "__main__":
+  main()
